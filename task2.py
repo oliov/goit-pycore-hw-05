87f7c6bb5369e32e9fedc52b3c5cb153208eb913
oliov <oleksii.vorobiov@keemail.me>
2025-10-27 14:24:41 +0200

    fixes crash on empty command

--- task2.py
+++ task2.py
@@ -5,13 +5,9 @@ def generator_numbers(text: str):
     numbers = re.findall(r'\d+.\d+|\d+', text)
     for num in numbers:
         yield float(num)
-    
 
 def sum_profit(text: str, func: Callable):
-    sum = 0
-    for number in func(text):
-        sum += number
-    return sum
+    return sum(func(text))
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
--- task4.py
+++ task4.py
@@ -14,33 +14,33 @@ def input_error(func):
             func_name = func.__name__
             is_add = func_name is add_contact.__name__
             is_change = func_name is change_contact.__name__
+            is_parser = func_name is parse_input.__name__
+            if is_parser:
+                if len(args[INPUT]) == 0:
+                    return 'error'
 
             if is_add or is_change: 
                 if len(args[INPUT]) != 2:
-                    print("Syntax: give me name and phone")
-                    return
+                    return "Syntax: give me name and phone"
                 is_user_exists = args[INPUT][USERNAME] in args[DATABASE] 
-
 
             if is_add:
                 if is_user_exists: 
-                    print("Contact already exists")
-                    return
+                    return "Contact already exists"
 
             if is_change:
                 if not is_user_exists:
-                    print("Contact doesn't exist")
-                    return
+                    return "Contact doesn't exist"
 
             return func(*args)
         except ValueError:
-            print("Give me name and phone please.")
+            return "Give me name and phone please."
         except KeyError:
-            print('Contact not found')
+            return 'Contact not found'
         except IndexError:
-            print('Index error')
+            return 'Index error'
         except Exception as err_msg:
-            print(err_msg)
+            return err_msg
     return inner
 
 def modify_database(args):
@@ -50,55 +50,56 @@ def modify_database(args):
 @input_error
 def add_contact(*args):
     modify_database(args)
-    print("Contact added")
+    return "Contact added"
 
 @input_error
 def change_contact(*args):
     modify_database(args)
-    print("Contact changed")
+    return "Contact changed"
     
 @input_error
 def show_phone(*args):
-    print(args[DATABASE][args[INPUT][USERNAME]])
+    return args[DATABASE][args[INPUT][USERNAME]]
         
 @input_error
 def show_all(*args):
     for name, phone in args[DATABASE].items():
-        print(f'{name}: {phone}')
-
-def exit_(*args):
-    print("Good bye!")
-    sys.exit()
+        return f'{name}: {phone}'
 
 def invalid_cmd(*args):
-    print("Invalid command")
-    return
+    return "Invalid command"
 
+@input_error
 def parse_input(user_input):
     cmd, *args = user_input.split()
     return cmd.casefold(), *args
 
 def hello(*args):
-    print('How can I help you?')
-
+    return 'How can I help you?'
 
 def main():
     contacts = {'test255': 255} #database
     
     # bot's functions dict
     run = {'add': add_contact, 'change': change_contact,
-            'phone':show_phone, 'all': show_all, 'exit': exit_,
-            'error': invalid_cmd, 'close': exit_, 'hello': hello
+            'phone':show_phone, 'all': show_all,
+            'error': invalid_cmd, 'hello': hello
     }
 
     while True:
 
         # waits for input, parses command and arguments, and runs function
         cmd, *args = parse_input(input("Hello Friend! #: "))
+
+        if cmd == 'close' or cmd == 'exit':
+            print("Good bye!")
+            break
+
         if cmd in run:
-            run[cmd](args, contacts)
+            print(run[cmd](args, contacts))
+
         else:
-            invalid_cmd()
+            print(invalid_cmd())
 
 if __name__ == '__main__':
     main()
